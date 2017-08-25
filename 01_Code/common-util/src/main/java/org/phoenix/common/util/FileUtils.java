cd8c673aaf339dac1a059f0cffc8f6916b0b08ea
qz <466284796@qq.com>
2017-08-25 17:58:49 +0800

    FileUtils.java and pom.xml include FileUtils

--- 01_Code/common-util/src/main/java/org/phoenix/common/util/FileUtils.java
+++ 01_Code/common-util/src/main/java/org/phoenix/common/util/FileUtils.java
@@ -1,9 +1,7 @@
 package org.phoenix.common.util;
 
-import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.RandomAccessFile;
+import java.util.List;
 
 import org.phoenix.common.constant.CharSetConst;
 
@@ -11,23 +9,17 @@ public class FileUtils {
 	public static final String file_separator = System
 			.getProperty("file.separator");
 
-	// 从文件中读取数据，以换行符分割，封装成一个字符串数组
-	public static String[] readDataArrayFromFile(String fileName, String encoder)
+	public static List<String> readLines(String filepath, String encoder)
 			throws Exception {
-		String contents = readDataFromFile(fileName, encoder);
-		return contents.split("[\r\n]+");
+		List<String> lines = org.apache.commons.io.FileUtils.readLines(
+				new File(filepath), encoder);
+		return lines;
 	}
 
-	// 从文件中读取数据
-	public static String readDataFromFile(String filepath, String encoder)
+	public static String readFileToString(String filepath, String encoder)
 			throws Exception {
-		BufferedInputStream bis = null;
-		try {
-			bis = new BufferedInputStream(new FileInputStream(filepath));
-			return StreamUtils.stream2str(bis, encoder);
-		} finally {
-			StreamUtils.closeStream(bis);
-		}
+		return org.apache.commons.io.FileUtils.readFileToString(new File(
+				filepath), encoder);
 	}
 
 	// 写入数据到指定文件
@@ -39,31 +31,38 @@ public class FileUtils {
 	// 写入数据到指定文件
 	public static void writeDataToFile(String filepath, String data,
 			String charset, boolean append) throws Exception {
-		writeByteToFile(filepath, data.getBytes(charset), append);
+		org.apache.commons.io.FileUtils.writeStringToFile(new File(filepath),
+				data, charset, append);
 	}
 
 	// 写入字节数组到指定文件
-	private static void writeByteToFile(String filepath, byte[] bytes,
-			boolean append) throws Exception {
-		File file = new File(filepath);
-		File p = file.getParentFile();
-		if (!p.exists()) {
-			p.mkdirs();
-		}
-		if (!append) {
-			file.delete();
-		}
+	// private static void writeByteToFile(String filepath, byte[] bytes,
+	// boolean append) throws Exception {
+	// File file = new File(filepath);
+	// File p = file.getParentFile();
+	// if (!p.exists()) {
+	// p.mkdirs();
+	// }
+	// if (!append) {
+	// file.delete();
+	// }
+	//
+	// RandomAccessFile raf = null;
+	// try {
+	// raf = new RandomAccessFile(filepath, "rw");
+	// if (append) {
+	// raf.seek(raf.length());
+	// }
+	// raf.write(bytes);
+	// } finally {
+	// StreamUtils.closeStream(raf);
+	// }
+	// }
 
-		RandomAccessFile raf = null;
-		try {
-			raf = new RandomAccessFile(filepath, "rw");
-			if (append) {
-				raf.seek(raf.length());
-			}
-			raf.write(bytes);
-		} finally {
-			StreamUtils.closeStream(raf);
-		}
+	public static void copyFile(String srcFile, String destFile)
+			throws Exception {
+		org.apache.commons.io.FileUtils.copyFile(new File(srcFile), new File(
+				destFile));
 	}
 
 	// 给定路径如果父目录不存在就创建
