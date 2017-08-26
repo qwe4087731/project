22fc803894b87562d9e30770c15440c59ea63604
qwe4087731 <qwe4087731@gmail.com>
2017-08-26 12:23:02 +0800

    commit classpath

--- 01_Code/common-util/src/main/java/org/phoenix/common/util/FileUtils.java
+++ 01_Code/common-util/src/main/java/org/phoenix/common/util/FileUtils.java
@@ -4,10 +4,73 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.RandomAccessFile;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
 
 public class FileUtils {
 	public static final String file_separator = System
 			.getProperty("file.separator");
+	private static Logger logger = Logger.getLogger(FileUtils.class);
+
+	public static Map<String, String> readMapFromFile(String fileName,
+			String encoder) throws Exception {
+		Map<String, String> map = new LinkedHashMap<String, String>();
+		try {
+			String[] lines = readDataArrayFromFile(fileName, encoder);
+			for (String line : lines) {
+				String[] values = line.split("=");
+				map.put(values[0], values[1]);
+			}
+		} catch (Exception e) {
+			logger.error("read map from file failed", e);
+		}
+		return map;
+	}
+
+	public static void writeMapToFile(String filepath, Map<String, String> map,
+			String encoder, boolean append) throws Exception {
+		if (map != null) {
+			StringBuilder buf = new StringBuilder();
+			for (Map.Entry<String, String> entry : map.entrySet()) {
+				buf.append(entry.getKey()).append("=").append(entry.getValue())
+						.append(file_separator);
+			}
+			if (buf.length() > 0) {
+				writeDataToFile(filepath, buf.toString(), encoder, append);
+			}
+		}
+	}
+
+	public static Set<String> readSetFromFile(String fileName, String encoder)
+			throws Exception {
+		Set<String> set = new LinkedHashSet<String>();
+		try {
+			String[] lines = readDataArrayFromFile(fileName, encoder);
+			for (String line : lines) {
+				set.add(line);
+			}
+		} catch (Exception e) {
+			logger.error("read set from file failed", e);
+		}
+		return set;
+	}
+
+	public static void writeSetToFile(String filepath, Set<String> set,
+			String encoder, boolean append) throws Exception {
+		if (set != null) {
+			StringBuilder buf = new StringBuilder();
+			for (String line : set) {
+				buf.append(line).append(file_separator);
+			}
+			if (buf.length() > 0) {
+				writeDataToFile(filepath, buf.toString(), encoder, append);
+			}
+		}
+	}
 
 	public static String[] readDataArrayFromFile(String fileName, String encoder)
 			throws Exception {
