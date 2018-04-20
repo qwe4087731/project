21805c88687e733a13ed958b9f88146c8efe4a06
qwe4087731 <qwe4087731@gmail.com>
2018-04-20 23:00:32 +0800

    create webapp-demo-parent project & webapp-demo module

---
+++ 01_Code/webapp-demo-parent/webapp-demo/src/main/java/com/smart/db/mybatis/dao/UserInfoDao.java
@@ -0,0 +1,10 @@
+package com.smart.db.mybatis.dao;
+
+import org.phoenix.dao.BaseDao;
+import org.springframework.stereotype.Repository;
+
+import com.smart.db.mybatis.bean.UserInfo;
+
+@Repository
+public interface UserInfoDao extends BaseDao<UserInfo> {
+}
