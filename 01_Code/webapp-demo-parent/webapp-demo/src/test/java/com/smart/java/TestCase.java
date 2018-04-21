32d837ecd7d6840c1453569118b42fd1e28dd796
qwe4087731 <qwe4087731@gmail.com>
2018-04-21 20:47:21 +0800

    add service testng

--- 01_Code/webapp-demo-parent/webapp-demo/src/test/java/com/smart/java/TestCase.java
+++ 01_Code/webapp-demo-parent/webapp-demo/src/test/java/com/smart/java/TestCase.java
@@ -3,21 +3,21 @@ package com.smart.java;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.smart.db.mybatis.bean.UserInfo;
-import com.smart.db.mybatis.dao.UserInfoDao;
-
+import com.smart.db.mybatis.service.UserInfoService;
 
 @ContextConfiguration("classpath*:/applicationContext.xml")
 public class TestCase extends AbstractTransactionalTestNGSpringContextTests {
-
 	@Autowired
-	private UserInfoDao userInfoDao;
+	private UserInfoService userInfoService;
 
 	@Test
 	public void testAddLoginLog() {
-		UserInfo userInfo = userInfoDao.selectByPrimaryKey(2);
-		System.out.println(userInfo.getUserName());
+		UserInfo userInfo = userInfoService.getUserInfo();
+		Assert.assertEquals(userInfo.getUserName(), "lisi");
+
 	}
 }
