3b4e6dfb489eb7b43cdcdd4937d71e642ee60d15
qwe4087731 <qwe4087731@gmail.com>
2018-04-26 23:53:46 +0800

    add @Repository

--- 01_Code/webapp-demo-parent/webapp-demo/src/main/java/com/smart/db/mybatis/dao/UserDao.java
+++ 01_Code/webapp-demo-parent/webapp-demo/src/main/java/com/smart/db/mybatis/dao/UserDao.java
@@ -1,8 +1,10 @@
 package com.smart.db.mybatis.dao;
 
 import org.phoenix.mybatis.dao.BaseDao;
+import org.springframework.stereotype.Repository;
 
 import com.smart.db.mybatis.bean.UserDO;
 
+@Repository
 public interface UserDao extends BaseDao<UserDO> {
 }
