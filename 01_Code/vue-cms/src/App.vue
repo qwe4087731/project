dd27ae6aa11892d858b010b7c6d624b34aea214f
qwe4087731 <qwe4087731@gmail.com>
2018-05-18 23:53:54 +0800

    add animate

--- 01_Code/vue-cms/src/App.vue
+++ 01_Code/vue-cms/src/App.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="app-container">
-        <mt-header fixed title="防守打法"></mt-header>
-        <router-view></router-view>
+        <mt-header fixed title="顶部导航"></mt-header>
+        <transition>
+            <router-view></router-view>
+        </transition>
         <nav class="mui-bar mui-bar-tab">
             <router-link class="mui-tab-item" to="/home">
                 <span class="mui-icon mui-icon-home"></span>
@@ -21,16 +23,6 @@
             </router-link>
         </nav>
     </div>
-
-    <!--<router-view></router-view>-->
-
-    <!--<div>-->
-    <!--<router-link to="/index"></router-link>-->
-    <!--<router-link to="/index"></router-link>-->
-    <!--<router-link to="/index"></router-link>-->
-    <!--<router-link to="/index"></router-link>-->
-    <!--</div>-->
-
 </template>
 
 <script>
@@ -52,5 +44,25 @@
 <style lang="scss" scoped>
     .app-container {
         padding-top: 40px;
+        // 一个组件向外移动，一个组件向内移动，此时设置如果宽度超过隐藏
+        overflow-x: hidden;
+    }
+
+    //v-enter和v-leave-totranslateX不一样，是为了让进入的从右边进去，出去的从左边出去
+    .v-enter {
+        opacity: 0;
+        transform: translateX(100%);
+    }
+
+    // 一个组件向外移动，一个组件向内移动，当同时2个组件都在屏幕内的时候，此时相当于向内移动的会放到向外移动的
+    //这里设置position是为了让向外移动的清除文档流，这样向内的就会在最上面开始进入。
+    .v-leave-to {
+        opacity: 0;
+        transform: translateX(-100%);
+        position: absolute;
+    }
+
+    .v-enter-active, .v-leave-active {
+        transition: all 0.5s ease;
     }
 </style>
--- 01_Code/vue-cms/src/component/HomeContainer.vue
+++ 01_Code/vue-cms/src/component/HomeContainer.vue
@@ -1,28 +1,74 @@
 <template>
     <div>
         <mt-swipe :auto="4000">
-            <mt-swipe-item>1</mt-swipe-item>
-            <mt-swipe-item>2</mt-swipe-item>
-            <mt-swipe-item>3</mt-swipe-item>
+            <mt-swipe-item v-for="item in lunboList" :key="item.url"><img :src="item.img"/></mt-swipe-item>
+            <!--<mt-swipe-item>2</mt-swipe-item>-->
         </mt-swipe>
-        <h3>HomeContainer</h3></div>
+        <ul class="mui-table-view mui-grid-view mui-grid-9">
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <span class="mui-icon mui-icon-home"></span>
+                <div class="mui-media-body">新闻资讯</div>
+            </a></li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <span class="mui-icon mui-icon-email"><span class="mui-badge">5</span></span>
+                <div class="mui-media-body">图片分享</div>
+            </a></li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <span class="mui-icon mui-icon-chatbubble"></span>
+                <div class="mui-media-body">商品购买</div>
+            </a></li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <span class="mui-icon mui-icon-location"></span>
+                <div class="mui-media-body">留言反馈</div>
+            </a></li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <span class="mui-icon mui-icon-search"></span>
+                <div class="mui-media-body">视频专区</div>
+            </a></li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <span class="mui-icon mui-icon-phone"></span>
+                <div class="mui-media-body">联系我们</div>
+            </a></li>
+        </ul>
+    </div>
 </template>
 
 <script>
+    import {Toast} from 'mint-ui'
+
     export default {
         name: "home",
         data: function () {
-            return {};
+            return {
+                lunboList: []
+
+            };
         },
         created: function () {
             this.getLunbotu();
         },
         methods: {
             getLunbotu: function () {
-                // this.$http.get("http://localhost/io/api/getlunbo").then(result => {
-                //     console.log(result.body);
-                // })
-                console.log(123);
+                // this.$http.get("http://localhost/io/api/getlunbo").then(response => {
+                //     console.log("success:");
+                //     console.log(response);
+                // }, response => {
+                //     console.log("error:");
+                //     console.log(response);
+                // });
+                if (true) {
+                    this.lunboList = [{
+                        'img': 'http://img.zcool.cn/community/018c3d59ac137da801211d25932cbd.jpg@1280w_1l_2o_100sh.png',
+                        'url': '1'
+                    }, {
+                        'img': 'http://img.zcool.cn/community/01080755c1edaf32f87528a18e9840.jpg@900w_1l_2o_100sh.jpg',
+                        'url': '2'
+                    }];
+                    console.log(this.lunboList);
+                } else {
+                    Toast('加载数据失败')
+                }
+
             }
         }
     }
@@ -41,6 +87,11 @@
             &:nth-child(3) {
                 background: green;
 
+            }
+
+            img {
+                width: 100%;
+                height: 100%;
             }
         }
     }
