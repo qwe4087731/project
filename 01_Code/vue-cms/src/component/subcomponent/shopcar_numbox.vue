dff1d1c2d3da4be6b34177235ebab011a8b51519
qwe4087731 <qwe4087731@gmail.com>
2018-05-29 22:54:31 +0800

    add  shopcar container

---
+++ 01_Code/vue-cms/src/component/subcomponent/shopcar_numbox.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="mui-numbox" data-numbox-min='1' style="height:25px;">
+        <button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
+        <input id="test" class="mui-input-numbox" type="number" value="1" @change="countChanged" ref="numbox"/>
+        <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
+    </div>
+</template>
+
+<script>
+    import mui from '../../lib/mui/js/mui.min.js'
+
+    export default {
+        name: "goodsinfo",
+        props: ["maxnum"],
+        mounted() {
+            mui('.mui-numbox').numbox()
+        },
+        methods: {}
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- 01_Code/vue-cms/src/component/tabbar/ShopcarContainer.vue
+++ 01_Code/vue-cms/src/component/tabbar/ShopcarContainer.vue
@@ -1,13 +1,103 @@
 <template>
-    <div><h3>ShopcarContainer</h3></div>
+    <div class="shopcar-container">
+        <div class="goods-list">
+
+            <div class="mui-card" v-for="item in shopcar" :key="item.id">
+                <div class="mui-card-content">
+                    <div class="mui-card-content-inner">
+                        <mt-switch></mt-switch>
+                        <img :src="item.img_url"/>
+                        <div clas="info">
+                            <h1>{{item.title}}</h1>
+                            <p>
+                                <span class="price">¥{{item.sell_price}}</span>
+                                <numbox></numbox>
+                                <a href="">删除</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
 </template>
 
 <script>
+    import numbox from '../subcomponent/shopcar_numbox.vue'
+
     export default {
-        name: "shopcar"
+        name: "shopcar",
+        data: {
+            shopcar: []
+        },
+        components: {
+            numbox
+        },
+        created() {
+            this.getShopCar();
+        },
+        methods: {
+            getShopCar() {
+                //传递参数ids，以逗号分割去获取购物车中的参数id
+                var ids = [];
+                this.$store.state.car.forEach(item =>
+                    ids.push(item.id)
+                )
+                if (ids.length <= 0) {
+                    return;
+                }
+                // this.$http.get("api/goods/getshopcarlist/" + ids.join(",")).then(result => {
+                //     if(result.body.status == 0){
+                //         this.goodslist = result.body.message;
+                //     }
+                // })
+                var array = [{
+                    id: 1,
+                    title: '华为(HUAWEI)荣耀PLUS 16G',
+                    img_url: 'http://img4.imgtn.bdimg.com/it/u=4063527091,1765948189&fm=27&gp=0.jpg',
+                    sell_price: 2195,
+                    count: 10
+                }, {
+                    id: 2,
+                    title: '小米6S',
+                    img_url: 'https://res.vmallres.com/pimages//sale/2018-04/2018042811131740.png',
+                    sell_price: 1800,
+                    count: 20
+                }]
+                this.shopcar = array;
+            }
+        }
     }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+    .shopcar-container {
+        background: #eee;
+        overflow: hidden;
+        .goods-list {
+            .mui-card-content-inner {
+                display: flex;
+                align-items: center;
+            }
+            img {
+                width: 60px;
+                height: 60px;
+            }
+            h1 {
+                font-size: 13px;
+            }
+            .info {
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+                .price {
+                    color: red;
+                    font-weight: bold;
+                }
+            }
 
+        }
+    }
 </style>
