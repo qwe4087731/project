4471cdaaabacfa6dc23e4bca049312907bcb904e
qz <466284796@qq.com>
2018-06-28 09:30:12 +0800

    add root user db auth

--- 02_DB/createdb.sql
+++ 02_DB/createdb.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE `book` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 CREATE USER 'remote_user'@'%' IDENTIFIED BY 'ouWFWdjy';
 GRANT ALL PRIVILEGES ON book.* TO 'remote_user'@'%';
+GRANT ALL PRIVILEGES ON book.* TO 'root'@'192.168.1.%';
