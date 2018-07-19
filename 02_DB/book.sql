5b717a93ebd3e220b5b2a3884b068d50396dae39
qwe4087731 <qwe4087731@gmail.com>
2018-07-19 22:26:05 +0800

    modify db

--- 02_DB/book.sql
+++ 02_DB/book.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2018/4/26 22:45:25                           */
+/* Created on:     2018/7/19 22:25:04                           */
 /*==============================================================*/
 
 
@@ -29,9 +29,9 @@ create table author
 (
    id                   int unsigned not null auto_increment,
    name                 varchar(32) not null,
-   intro                varchar(64) not null,
-   gmt_create           datetime not null,
-   gmt_modified         datetime not null,
+   intro                varchar(64) not null default "",
+   gmt_create           timestamp not null default CURRENT_TIMESTAMP,
+   gmt_modified         timestamp not null default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
    primary key (id)
 );
 
@@ -43,11 +43,11 @@ create table book
    id                   int unsigned not null auto_increment,
    name                 varchar(128) not null,
    author_id            int unsigned not null,
-   author_name          varbinary(32) not null,
+   author_name          varchar(32) not null,
    intro                varchar(256) not null,
    intro_detail         varchar(256) not null,
-   gmt_create           datetime not null,
-   gmt_modified         datetime not null,
+   gmt_create           timestamp not null default CURRENT_TIMESTAMP,
+   gmt_modified         timestamp not null default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
    primary key (id)
 );
 
@@ -69,8 +69,8 @@ create table oauth
    oauth_type           tinyint unsigned not null,
    oauth_id             varchar(64) not null,
    oauth_token          varchar(64) not null,
-   gmt_create           datetime not null,
-   gmt_modified         datetime not null,
+   gmt_create           timestamp not null default CURRENT_TIMESTAMP,
+   gmt_modified         timestamp not null default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
    primary key (id)
 );
 
@@ -91,12 +91,13 @@ create table section
    id                   int unsigned not null auto_increment,
    book_id              int unsigned not null,
    ordered              int unsigned not null,
-   section_num          int unsigned not null,
+   tag                  varchar(32),
+   section_num          varchar(16) not null,
    section_name         varchar(64) not null,
    part_num             int unsigned not null default 0,
    part_name            varchar(64) not null default "",
-   gmt_create           datetime not null,
-   gmt_modified         datetime not null,
+   gmt_create           timestamp not null default CURRENT_TIMESTAMP,
+   gmt_modified         timestamp not null default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
    primary key (id)
 );
 
@@ -116,8 +117,8 @@ create table section_content
 (
    section_id           int unsigned not null,
    content              text not null,
-   gmt_create           datetime not null,
-   gmt_modified         datetime not null,
+   gmt_create           timestamp not null default CURRENT_TIMESTAMP,
+   gmt_modified         timestamp not null default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
    primary key (section_id)
 );
 
@@ -129,8 +130,8 @@ create table user
    id                   int unsigned not null auto_increment,
    name                 varchar(32) not null,
    img_id               int unsigned not null,
-   gmt_create           datetime not null,
-   gmt_modified         datetime not null,
+   gmt_create           timestamp not null default CURRENT_TIMESTAMP,
+   gmt_modified         timestamp not null default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
    primary key (id)
 );
 
